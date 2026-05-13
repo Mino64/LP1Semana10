400d88b8a6bffe1216f3b2ed8d9db5b60db2ead4
Daniel <daniel.henriques.gamedev@gmail.com>
2026-05-13 16:35:29 +0100

    IntCollections

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace IntCollections
 {
@@ -6,7 +7,25 @@ namespace IntCollections
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            List<int> listy = new List<int>();
+            Stack<int> stacky = new Stack<int>();
+            Queue<int> queuey = new Queue<int>();
+            HashSet<int> hashSety = new HashSet<int>();
+
+            int[] values = {1,10, -30, 10, -5};
+
+            foreach (int num in values)
+            {
+                listy.Add(num);
+                stacky.Push(num);
+                queuey.Enqueue(num);
+                hashSety.Add(num);
+            }
+
+            Console.WriteLine($"List: {string.Join(", ", listy)}");
+            Console.WriteLine($"Stack: {string.Join(", ", stacky)}");
+            Console.WriteLine($"Queue: {string.Join(", ", queuey)}");
+            Console.WriteLine($"HashSet: {string.Join(", ", hashSety)}");
         }
     }
 }
