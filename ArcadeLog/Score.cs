089bb1a2d3f2a4013e5b0b86c6bdd9b61d354381
Daniel <daniel.henriques.gamedev@gmail.com>
2026-05-14 16:15:18 +0100

    Score class complete

--- ArcadeLog/Score.cs
+++ ArcadeLog/Score.cs
@@ -5,30 +5,69 @@ namespace ArcadeLog
     public class Score : IComparable<Score>
     {
         // Variável de Instância Privada: points (int)
-        // CÓDIGO AQUI
+        // done
+        private int points;
 
         // Propriedade Auto-Implementada Só de Leitura: Name (string)
-        // CÓDIGO AQUI
-
+        // done
+        public string Name { get; }
         // Propriedade: Points (int), sempre entre 0 e 9999
-        // CÓDIGO AQUI
-
+        // 
+        public int Points
+        {
+            get
+            {
+                if (points >= 9999) return 9999;
+                if (points <= 0) return 0;
+                return points;
+            }
+            set
+            {
+                points = value;
+                if(points < 0) points = 0;
+            }
+        }
         // Propriedade Só de Leitura: Medal (string)
-        // CÓDIGO AQUI
+        // done
+
+        public string Medal
+        {
+            get
+            {
+                switch (points)
+                {
+                    case >= 7000:
+                        return "Gold";
+
+                    case >= 4000:
+                        return "Silver";
+
+                    default:
+                        return "Bronze";
+                }
+            }
+        }
 
         // Construtor: aceita nome e pontuação
         // CÓDIGO AQUI
 
+        public Score(string name, int score)
+        {
+            Name = name;
+            Points = score;
+        }
+
         public int CompareTo(Score other)
         {
             // CÓDIGO AQUI
-            return 0; // substitua o return
+            if(other is null) return 1;
+            return other.points - this.points; // substitua o return
         }
 
         public override string ToString()
         {
             // CÓDIGO AQUI
-            return ""; // substitua o return
+            return $"{Name} [{Medal}]: {Points}"; // substitua o return
         }
     }
 }
